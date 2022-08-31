f4e11f624e6a92d1c02ac52258ac4e72dc93c4b4
stéfany <stelar.dev@gmail.com>
2022-08-31 12:29:37 -0300

    countries ok

---
+++ countries.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch');
+
+function getCountriesData() {
+  const endpoint = 'https://restcountries.com/v3.1/all';
+
+  return fetch(endpoint)
+    .then(data => data.json())
+}
+
+async function getNames() {
+  const countriesNames = await getCountriesData()
+    .then(data => data.map(({ name }) => name.common));
+
+  return countriesNames;
+}
+
+async function getCapitals() {
+  const countriesCapitals = await getCountriesData()
+    .then(data => data.map(({ capital }) => capital || '-'));
+
+  return countriesCapitals;
+}
+
+async function getAreas() {
+  const countriesAreas = await getCountriesData()
+    .then(data => data.map(({ area }) => area));
+
+  return countriesAreas;
+}
+
+async function getCurrencies() {
+  const countriesCurrencies = await getCountriesData()
+    .then(data => data.map(({ currencies }) => {
+      let code = ['-'];
+
+      if (currencies) {
+        code = Object.keys(currencies);
+      }
+
+      return code;
+    }))
+
+  return countriesCurrencies;
+}
+
+module.exports = { getAreas, getCapitals, getCurrencies, getNames, getCountriesData }
+// getNames();
+// getCapitals();
+// getAreas();
+
